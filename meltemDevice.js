b29beb395c99b6c7d42b24f8a57276175b309dcf
xtra72 <xtra72@gmail.com>
2018-02-17 10:59:42 +0900

    정보 오류 수정 - D00으로 가져오는 RPM은 설정 RPM. - D80 명령을 추가하여 현재값으로 변경 설정 하기 변경 - 설정 관려 코드 변경

--- meltemDevice.js
+++ meltemDevice.js
@@ -4,118 +4,57 @@ var util = require('util');
 var _ = require('lodash');
 var EventEmitter = require('events').EventEmitter;
 var logger = require('./index').Sensor.getLogger('Sensor');
-var STATUS_FIELDS = ['mode', 'speed', 'current', 'pressure', 'temperature'];
-var SETTINGS_COMMON_S01_INDEX = 0;
-var SETTINGS_COMMON_S02_INDEX = 1;
-var SETTINGS_COMMON_S03_INDEX = 2;
-var SETTINGS_COMMON_S04_INDEX = 3;
-var SETTINGS_COMMON_S05_INDEX = 4;
-var SETTINGS_COMMON_S06_INDEX = 5;
-var SETTINGS_COMMON_S07_INDEX = 6;
-var SETTINGS_COMMON_S08_INDEX = 7;
-var SETTINGS_COMMON_S09_INDEX = 8;
-var SETTINGS_COMMON_S10_INDEX = 9;
-var SETTINGS_COMMON_S11_INDEX = 10;
-var SETTINGS_COMMON_S12_INDEX = 11;
-var SETTINGS_SET1_S13_INDEX = 12;
-var SETTINGS_SET1_S14_INDEX = 13;
-var SETTINGS_SET1_S15_INDEX = 14;
-var SETTINGS_SET1_S16_INDEX = 15;
-var SETTINGS_SET2_S17_INDEX = 16;
-var SETTINGS_SET2_S18_INDEX = 17;
-var SETTINGS_SET2_S19_INDEX = 18;
-var SETTINGS_SET2_S20_INDEX = 19;
-var SETTINGS_SET2_S21_INDEX = 20;
-var SETTINGS_SET3_S22_INDEX = 21;
-var SETTINGS_SET3_S23_INDEX = 22;
-var SETTINGS_SET3_S24_INDEX = 23;
-var SETTINGS_SET1_RPM_INDEX = 24;
-var SETTINGS_SET1_CURRENT_INDEX = 25;
-var SETTINGS_SET2_RPM_INDEX = 26;
-var SETTINGS_SET2_CURRENT_INDEX = 27;
-var SETTINGS_FIXED_RPM_INDEX = 28;
-
-var DEFAULT_SETTINGS= [
-  { value: 0, new: false, setCommand: 'S01' }, // common.s01
-  { value: 3, new: false, setCommand: 'S02' }, // common.s02
-  { value: 0, new: false, setCommand: 'S03' }, // common.s03
-  { value: 3, new: false, setCommand: 'S04' }, // common.s04
-  { value: 270, new: false, setCommand: 'S05' }, // common.s05
-  { value: 100, new: false, setCommand: 'S06' }, // common.s06
-  { value: 80, new: false, setCommand: 'S07' }, // common.s07
-  { value: 60, new: false, setCommand: 'S08' }, // common.s08
-  { value: 40, new: false, setCommand: 'S09' }, // common.s09
-  { value: 5, new: false, setCommand: 'S10' }, // common.s10
-  { value: 1, new: false, setCommand: 'S11' }, // common.s11
-  { value: 1440, new: false, setCommand: 'S12' }, // common.s12
-  { value: -50, new: false, setCommand: 'S13' }, // set1.s13
-  { value: 2, new: false, setCommand: 'S14' }, // set1.s14
-  { value: 10, new: false, setCommand: 'S15' },  // set1.s15
-  { value: 5, new: false, setCommand: 'S16' }, // set1.s16
-  { value: -210, new: false, setCommand: 'S17' }, // set2.s17
-  { value: 3, new: false, setCommand: 'S18' }, // set2.s18
-  { value: 50, new: false, setCommand: 'S19' }, // set2.s19
-  { value: 20, new: false, setCommand: 'S20' }, // set2.s20
-  { value: 50, new: false, setCommand: 'S21' }, // set2.s21
-  { value: -380, new: false, setCommand: 'S22' }, // set3.s22
-  { value: 10, new: false, setCommand: 'S23' }, // set3.s23
-  { value: 30, new: false, setCommand: 'S24' },// set3.s24
-  { value: 580, new: false, setCommand: 'T01' },  // set1_rpm
-  { value: 74, new: false, setCommand: 'T02' }, // set1_current
-  { value: 760, new: false, setCommand: 'T03' },  // set2_rpm
-  { value: 431, new: false, setCommand: 'T04'}, // set2_current
-  { value: 850, new: false, command: 'T05'} // fixed_rpm
-];
-
-function MeltemCVSDeviceOnData(self, parsedPayload) {
-  STATUS_FIELDS.map(function (field) {
-    var item = {};
-
-    item.sequence = field;
-    switch (field) {
-      case 'mode': item.value = parsedPayload.mode; break;
-      case 'pressure': item.value = parsedPayload.pressure; break;
-      case 'speed': item.value = parsedPayload.speed; break;
-      case 'current': item.value = parsedPayload.current; break;
-      case 'power': item.value = parsedPayload.power; break;
-      case 'temperature': item.value = parsedPayload.temperature; break;
-      default: return;
-    }
-
-    self.master.emit(parsedPayload.id + '-' + field, item);
-  });
-}
+var STATUS_FIELDS = ['mode', 'speed', 'current', 'pressure', 'power', 'temperature'];
 
 function MeltemCVSDevice(master, id) {
   var self = this;
 
   self.id = id;
   self.master = master;
-  self.settings = DEFAULT_SETTINGS;
+  self.settings['s01'] = { value: 0, new: false, setCommand: 'S01'};
+  self.settings['s02'] = { value: 3, new: false, setCommand: 'S02'};
+  self.settings['s03'] = { value: 0, new: false, setCommand: 'S03'};
+  self.settings['s04'] = { value: 3, new: false, setCommand: 'S04'};
+  self.settings['s05'] = { value: 270, new: false, setCommand: 'S05'};
+  self.settings['s06'] = { value: 100, new: false, setCommand: 'S06'};
+  self.settings['s07'] = { value: 80, new: false, setCommand: 'S07'};
+  self.settings['s08'] = { value: 60, new: false, setCommand: 'S08'};
+  self.settings['s09'] = { value: 40, new: false, setCommand: 'S09'};
+  self.settings['s10'] = { value: 5, new: false, setCommand: 'S10'};
+  self.settings['s11'] = { value: 1, new: false, setCommand: 'S11'};
+  self.settings['s12'] = { value: 1440, new: false, setCommand: 'S12'};
+  self.settings['s13'] = { value: -50, new: false, setCommand: 'S13'};
+  self.settings['s14'] = { value: 2, new: false, setCommand: 'S14'};
+  self.settings['s15'] = { value: 10, new: false, setCommand: 'S15'};
+  self.settings['s16'] = { value: 5, new: false, setCommand: 'S16'};
+  self.settings['s17'] = { value: -210, new: false, setCommand: 'S17'};
+  self.settings['s18'] = { value: 3, new: false, setCommand: 'S18'};
+  self.settings['s19'] = { value: 50, new: false, setCommand: 'S19'};
+  self.settings['s20'] = { value: 20, new: false, setCommand: 'S20'};
+  self.settings['s21'] = { value: 50, new: false, setCommand: 'S21'};
+  self.settings['s22'] = { value: -380, new: false, setCommand: 'S22'};
+  self.settings['s23'] = { value: 10, new: false, setCommand: 'S23'};
+  self.settings['s24'] = { value: 30, new: false, setCommand: 'S24'};
+  self.settings['set1_rpm'] = { value: 580, new: false, setCommand: 'T01'};
+  self.settings['set1_current'] = { value: 74, new: false, setCommand: 'T02'};
+  self.settings['set2_rpm'] = { value: 760, new: false, setCommand: 'T03'};
+  self.settings['set2_current'] = { value: 431, new: false, setCommand: 'T04'};
+  self.settings['set3_rpm'] = { value: 760, new: false, setCommand: 'T05'};
+  self.settings['set3_current'] = { value: 431, new: false, setCommand: 'T06'};
+  self.settings['fixed_rpm'] = { value: 850, new: false, setCommand: 'R00'};
+
   self.seriesCommandSets = [ 
     { 
       command : 'D00', 
-      timeout : self.master.responseWaitingTime,
-      onData : function(parsedPayload) {
-        if (parsedPayload.items.length == 6) {
-          MeltemCVSDeviceOnData(self, parsedPayload);
-        }
-      }
+      timeout : self.master.responseWaitingTime
     }, 
     { 
-      command : 'D01', 
-      timeout : self.master.responseWaitingTime,
-      onData : function(parsedPayload) {
-      }
+      command : 'D80', 
+      timeout : self.master.responseWaitingTime
     }, 
     {
       command : 'F01',
-      timeout : self.master.responseWaitingTime,
-      onData : function(parsedPayload) {
-        if (parsedPayload.items.length == 3) {
-          MeltemCVSDeviceOnData(self, parsedPayload);
-        }
-      }
+      timeout : self.master.responseWaitingTime
     }
   ];
   self.setCommandSets = [];
@@ -134,19 +73,10 @@ function MeltemCVSDevice(master, id) {
       var data = new Buffer(payload).toString().replace(/[\n\r]+/g, '');
       logger.trace('[Meltem CVS Device] onData():', data);
 
-      var parsedPayload = ParsePayload(data);
-      if ((parsedPayload instanceof Error) || (parsedPayload.id != self.id)) {
-        logger.error(parsedPayload);
-        return;
-      }
-
       clearTimeout(self.timeoutID);
       self.timeoutID = undefined;
 
-      if (self.commandSet.onData != undefined) {
-        self.commandSet.onData(parsedPayload);
-      }
-
+      self.onData(self.commandSet.command, data);
       self.commandSet = undefined;
 
       if (self.commandSets.length != 0) {
@@ -171,59 +101,66 @@ function MeltemCVSDevice(master, id) {
 
   self.on('settings', function(settings){
 
-    self.setValue(SETTINGS_COMMON_S01_INDEX, settings.s01);
-    self.setValue(SETTINGS_COMMON_S02_INDEX, settings.s02);
-    self.setValue(SETTINGS_COMMON_S03_INDEX, settings.s03);
-    self.setValue(SETTINGS_COMMON_S04_INDEX, settings.s04);
-    self.setValue(SETTINGS_COMMON_S05_INDEX, settings.s05);
-    self.setValue(SETTINGS_COMMON_S06_INDEX, settings.s06);
-    self.setValue(SETTINGS_COMMON_S07_INDEX, settings.s07);
-    self.setValue(SETTINGS_COMMON_S08_INDEX, settings.s08);
-    self.setValue(SETTINGS_COMMON_S09_INDEX, settings.s09);
-    self.setValue(SETTINGS_COMMON_S10_INDEX, settings.s10);
-    self.setValue(SETTINGS_COMMON_S11_INDEX, settings.s11);
-    self.setValue(SETTINGS_COMMON_S12_INDEX, settings.s12);
-    self.setValue(SETTINGS_SET1_S13_INDEX, settings.s13);
-    self.setValue(SETTINGS_SET1_S14_INDEX, settings.s14);
-    self.setValue(SETTINGS_SET1_S15_INDEX, settings.s15);
-    self.setValue(SETTINGS_SET1_S16_INDEX, settings.s16);
-    self.setValue(SETTINGS_SET2_S17_INDEX, settings.s17);
-    self.setValue(SETTINGS_SET2_S18_INDEX, settings.s18);
-    self.setValue(SETTINGS_SET2_S19_INDEX, settings.s19);
-    self.setValue(SETTINGS_SET2_S20_INDEX, settings.s20);
-    self.setValue(SETTINGS_SET2_S21_INDEX, settings.s21);
-    self.setValue(SETTINGS_SET3_S22_INDEX, settings.s22);
-    self.setValue(SETTINGS_SET3_S23_INDEX, settings.s23);
-    self.setValue(SETTINGS_SET3_S24_INDEX, settings.s24);
-    self.setValue(SETTINGS_SET1_RPM, settings.set1_rpm);
-    self.setValue(SETTINGS_SET1_CURRENT, settings.set1_current);
-    self.setValue(SETTINGS_SET2_RPM, settings.set2_rpm);
-    self.setValue(SETTINGS_SET2_CURRENT, settings.set2_current);
-    self.setValue(SETTINGS_FIXED_RPM.fixed_rpm);
+    //if ( settings.s01 != undefined ) self.setValue('s01', settings.s01);
+    if ( settings.s02 != undefined ) self.setValue('s02', settings.s02);
+    if ( settings.s03 != undefined ) self.setValue('s03', settings.s03);
+    if ( settings.s04 != undefined ) self.setValue('s04', settings.s04);
+    if ( settings.s05 != undefined ) self.setValue('s05', settings.s05);
+    if ( settings.s06 != undefined ) self.setValue('s06', settings.s06);
+    if ( settings.s07 != undefined ) self.setValue('s07', settings.s07);
+    if ( settings.s08 != undefined ) self.setValue('s08', settings.s08);
+    if ( settings.s09 != undefined ) self.setValue('s09', settings.s09);
+    if ( settings.s10 != undefined ) self.setValue('s10', settings.s10);
+    if ( settings.s11 != undefined ) self.setValue('s11', settings.s11);
+    if ( settings.s12 != undefined ) self.setValue('s12', settings.s12);
+    if ( settings.s13 != undefined ) self.setValue('s13', settings.s13);
+    if ( settings.s14 != undefined ) self.setValue('s14', settings.s14);
+    if ( settings.s15 != undefined ) self.setValue('s15', settings.s15);
+    if ( settings.s16 != undefined ) self.setValue('s16', settings.s16);
+    if ( settings.s17 != undefined ) self.setValue('s17', settings.s17);
+    if ( settings.s18 != undefined ) self.setValue('s18', settings.s18);
+    if ( settings.s19 != undefined ) self.setValue('s19', settings.s19);
+    if ( settings.s20 != undefined ) self.setValue('s20', settings.s20);
+    if ( settings.s21 != undefined ) self.setValue('s21', settings.s21);
+    if ( settings.s22 != undefined ) self.setValue('s22', settings.s22);
+    if ( settings.s23 != undefined ) self.setValue('s23', settings.s23);
+    if ( settings.s24 != undefined ) self.setValue('s24', settings.s24);
+    if ( settings.set1 != undefined ) {
+      if (settings.set1.rpm != undefined) self.setValue('set1_rpm', settings.set1.rpm);
+      if (settings.set1.current != undefined) self.setValue('set1_current', settings.set1.current);
+    }
+    if ( settings.set2 != undefined ) {
+      if (settings.set2.rpm != undefined) self.setValue('set2_rpm', settings.set2.rpm);
+      if (settings.set2.current != undefined) self.setValue('set2_current', settings.set2.current);
+    }
+    if ( settings.set3 != undefined ) {
+      if (settings.set3.rpm != undefined) self.setValue('set3_rpm', settings.set3.rpm);
+      if (settings.set3.current != undefined) self.setValue('set3_current', settings.set3.current);
+    }
+    if ( settings.rpm != undefined ) self.setValue('fixed_rpm', settings.rpm);
 
-    logger.trace('New Settings : ', self.newSettings);
     logger.trace('Settings : ', self.settings);
   });
 }
 
 util.inherits(MeltemCVSDevice, EventEmitter);
 
-MeltemCVSDevice.prototype.setValue = function(field, value) {
+MeltemCVSDevice.prototype.setValue = function(field, setting) {
   var self = this;
   var i;
 
-  if (value != undefined) {
-    self.settings[field].value = value;
+  if ((self.settings[field] != undefined) || ((setting != undefined) && (setting.value != undefined))) {
+    self.settings[field].value = setting.value;
     self.settings[field].new = true;
 
     for (i = 0; i < self.setCommandSets.length; i++) {
-      if (self.setCommandSets[i].command == DEFAULT_SETTINGS[field].command) {
+      if (self.setCommandSets[i].command == self.settings[field].setCommand) {
         self.setCommandSets[i].value = value;
         return;
       }
     }
 
-    self.setCommandSets.push({ command: DEFAULT_SETTINGS[field].setCommand, timeout: self.master.responseWaitingTime });
+    self.setCommandSets.push({ command: self.settings[field].setCommand, timeout: self.master.responseWaitingTime });
   }
 }
 
@@ -250,15 +187,86 @@ MeltemCVSDevice.prototype.onRequest = function(commandSet) {
 }
 
 MeltemCVSDevice.prototype.onUpdateSettings = function(timeout) {
+  var self = this;
   var i;
 
-  if (self.newSettings == 0) {
-    return;
-  }
 
   for(i = 0 ; i < self.settings.length ; i++) {
     if (self.settings[i].new) {
     }
+  }
+}
+
+MeltemCVSDevice.prototype.onData  = function(cmd, payload) {
+  var self = this;
+  var item = {};
+  var dataArray = payload.split(' ');
+
+  try {
+    switch (cmd) {
+      case 'D00':
+        {
+          var mode;
+          var pressure;
+          var current;
+          if ((payload.length != 28) || (dataArray.length != 6) || (self.id != dataArray[1])) {
+            throw 'Invalid payload : ' + payload;
+          }
+
+          mode = dataArray[2];
+          pressure = parseInt(dataArray[3].substr(0, 4));
+          if (pressure == NaN) {
+            throw 'Invalid field[3] : ' + dataArray[3];
+          }
+
+          current = parseInt(dataArray[5].substr(0, 4));
+          if (current == NaN) {
+            throw 'Invalid field[4] : ' + dataArray[5];
+          }
+
+          self.master.emit(self.id + '-mode', { sequence: 'mode', value: mode });
+          self.master.emit(self.id + '-pressure', { sequence: 'pressure', value: pressure });
+          self.master.emit(self.id + '-current', { sequence: 'current', value: current });
+          self.master.emit(self.id + '-power', { sequence: 'power', value: (current * 2 * 200 / 1000) });
+        }
+        break;
+
+      case 'D80':
+        {
+          if ((payload.length != 13) || (dataArray.length != 3) || (self.id != dataArray[1])) {
+            throw 'Invalid payload : ' + payload;
+          }
+
+          var speed = parseInt(dataArray[2].substr(0, 4));
+          if (speed == NaN) {
+            throw 'Invalid field[2] : ' + dataArray[2];
+          }
+
+          self.master.emit(self.id + '-speed', { sequence: 'speed', value: speed });
+        }
+        break;
+
+      case 'F01':
+        {
+          if ((payload.length != 13) || (dataArray.length != 3) || (self.id != dataArray[1])) {
+            throw 'Invalid payload : ' + payload;
+          }
+
+          var temperature = parseInt(dataArray[2].substr(0, 5));
+          if (temperature == NaN) {
+            throw 'Invalid field[2] : ' + dataArray[2];
+          }
+
+          self.master.emit(self.id + '-temperature', { sequence: 'temperature', value: temperature });
+        }
+        break;
+
+      default:
+        return;
+    }
+  }
+  catch (err) {
+    logger.error(err);
   }
 }
 
@@ -269,63 +277,6 @@ function CreateInstance(master, id) {
 
    return instance;
  }
-
-function ParsePayload(payload) {
-  var result = {};
-  var dataArray = payload.split(' ');
-  var error;
-
-  try {
-    if (payload.length == 28) {
-      if (dataArray.length != 6) {
-        throw 'Invalid payload : ' + payload;
-      }
-
-      result.id = dataArray[1];
-      result.mode = dataArray[2];
-      result.pressure = parseInt(dataArray[3].substr(0, 4));
-      if (result.pressure == NaN) {
-        throw 'Invalid field[3] : ' + dataArray[3];
-      }
-
-      result.speed = parseInt(dataArray[4].substr(0, 4));
-      if (result.speed == NaN) {
-        throw 'Invalid field[4] : ' + dataArray[4];
-      }
-
-      result.current = parseInt(dataArray[5].substr(0, 4));
-      if (result.current == NaN) {
-        result.power = NaN;
-        throw 'Invalid field[4] : ' + dataArray[5];
-      }
-      else {
-        result.power = result.current * 2 * 220 / 1000;
-      }
-    }
-    else if (payload.length == 13) {
-      if (dataArray.length != 3) {
-        throw 'Invalid payload : ' + payload;
-      }
-
-      result.id = dataArray[1];
-      result.temperature = parseInt(dataArray[2].substr(0, 5));
-      if (result.pressure == NaN) {
-        throw 'Invalid field[3] : ' + dataArray[2];
-      }
-    }
-    else {
-        throw 'Invalid payload : ' + payload;
-    }
-
-    result.items = dataArray;
-    logger.trace('Parsed:', result);
-  }
-  catch (err) {
-    error = new Error(err);
-  }
-
-  return error || result;
-}
 
 module.exports = {
    create: CreateInstance
